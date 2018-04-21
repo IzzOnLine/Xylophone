934c40099a8dff5fe30f02aa838872309ccb1eab
Stefania Izzo <stefania.izzo@sorint.it>
2018-04-21 10:39:53 +0200

    Start implementing sound

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -2,6 +2,10 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController{
+    
+
+    // AVAudioPlayer object
+    var player: AVAudioPlayer?
 
     
     override func viewDidLoad() {
@@ -11,10 +15,28 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        // reference to a wave sound location
+        let url = Bundle.main.url(forResource: "note1", withExtension: "wav")
         
-        print(sender.tag);
-        
-        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            
+            
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url!, fileTypeHint: AVFileType.wav.rawValue)
+            
+            /* iOS 10 and earlier require the following line:
+             player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
     
   
