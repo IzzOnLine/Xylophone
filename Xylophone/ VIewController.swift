c05e3b05bdf7e85b0f88186820e9f6836d957fe6
Stefania Izzo <stefania.izzo@sorint.it>
2018-04-21 09:59:51 +0200

    first commit

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        print(sender.tag);
+        
+        
+    }
+    
+  
+
+}
+
